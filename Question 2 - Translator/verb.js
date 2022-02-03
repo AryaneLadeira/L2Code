ff5c93e4d1c208ae5683ae81f19de73d353b10cd
Aryane Ladeira <69313233+AryaneLadeira@users.noreply.github.com>
2022-02-03 14:02:13 -0300

    Verificando e tratando as palavras de acordo com o verbo
    Verificando as palavras de acordo com os verbos presentes em um array de objetos e fazendo uma tratativa caso a palavra se encaixe em mais de um.

--- Question 2 - Translator/verb.js
+++ Question 2 - Translator/verb.js
@@ -3,3 +3,66 @@ const data = fs.readFileSync("verb.in.txt", "utf8");
 const wordsArray = data.split("\n").map((word) => word.replace("\r", ""));
 
 console.log(wordsArray)
+
+const verbs = [{
+    tense: "present",
+    persons: ["o", "os", "a", "om", "ons", "am"],
+},
+{
+    tense: "past",
+    persons: ["ei", "es", "e", "em", "est", "im"],
+},
+{
+    tense: "future",
+    persons: ["ai", "ais", "i", "aem", "aist", "aim"],
+}];
+
+const persons = ["1st", "2nd", "3rd", "4th", "5th", "6th"]
+
+const verifyWords = () => {
+    const verifiedWords = [];
+
+    wordsArray.forEach((word) => {
+        let found = false;
+
+        verbs.forEach((verb, tenseIndex) => {
+            const personIndex = verb.persons.findIndex((person) => word.endsWith(person));
+
+            if(personIndex !== -1){
+                found = true;
+
+                const wordObject = {
+                    word: word,
+                    isAVerb: true,
+                    tense: verb.tense,
+                    tenseIndex: tenseIndex,
+                    person: personIndex,
+                    ending: verbs[tenseIndex].persons[personIndex]
+                }
+
+                const duplicateIndex = verifiedWords.findIndex((object) => {
+                    return object.word === word;
+                })
+
+                if(duplicateIndex !== -1){
+
+                    if (wordObject.ending.length > verifiedWords[duplicateIndex].ending.length)
+                        verifiedWords[duplicateIndex] = wordObject;
+    
+                }else{
+                    verifiedWords.push(wordObject)
+                }
+            }
+        })
+
+        if(found === false){
+            verifiedWords.push({
+                word: word,
+                isAVerb: false,
+            })
+        }
+
+    });
+};
+
+verifyWords(wordsArray);
