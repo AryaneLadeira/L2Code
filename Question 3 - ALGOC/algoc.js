3ef5dde0ab6ab7e40b052b8afdd8409e6b31c94d
Aryane Ladeira <69313233+AryaneLadeira@users.noreply.github.com>
2022-02-03 22:15:18 -0300

    Lógica e funções para adicionar ou diminuir otimizada feita

--- Question 3 - ALGOC/algoc.js
+++ Question 3 - ALGOC/algoc.js
@@ -6,24 +6,66 @@ const plusOrMinus = (constant) => {
     return constant > 0 ? {constant: 1, instruction: "PLUSONE"} : {constant: -1, instruction: "MINUSONE"}
 }
 
+const inc = (constant) => {
+    return {constant: constant + 1, instruction: "INC"}
+}
+
+const dup = (constant) => {
+    return {constant: constant * 2, instruction: "DUP"}
+}
+
 const verifyConstant = () => {
+    const constantsVerifieds = [];
     for(let i = 0; i < constantsArray.length; i++){
         if(constantsArray[i] === 0){
             break
         }
+        
+        if(constantsArray[i] > 32767 || constantsArray[i] < -32768){
+            console.lop(`Constante ${constantsArray[i]} fora do escopo.`)
+        }else {
+            const constantCreated = {
+                constant: constantsArray[i],
+                actualConstant: 0,
+                instructions: []
+            }
+    
+            const response = plusOrMinus(constantCreated.constant);
+            constantCreated.actualConstant = response.constant;
+            constantCreated.instructions.push(response.instruction);
+    
+            while(true){
+                if(constantCreated.actualConstant === constantCreated.constant){
+                    break;
+                }
 
-        const constantCreated = {
-            constant: constantsArray[i],
-            actualConstant: 0,
-            instructions: []
+                let response;
+
+                if(constantCreated.actualConstant < 0){
+                    if(constantCreated.actualConstant - 1 === constantCreated.constant){
+                        response = inc(constantCreated.actualConstant)
+                    }else{
+                        (constantCreated.actualConstant + 1 > constantCreated.constant)?
+                            response = dup(constantCreated.actualConstant)
+                        :
+                        response = inc(constantCreated.actualConstant)
+                    }
+                }else{
+                    ((constantCreated.actualConstant === 1 || constantCreated.actualConstant % 2 !== 0) && constantCreated.actualConstant * 2 <= constantCreated.constant)?
+                        response = dup(constantCreated.actualConstant)
+                    :
+                        response = inc(constantCreated.actualConstant)
+                }
+                constantCreated.actualConstant = response.constant;
+                constantCreated.instructions.push(response.instruction)
+            }
+
+            constantsVerifieds.push(constantCreated)
         }
 
-        const response = plusOrMinus(constantCreated.constant);
-        constantCreated.actualConstant = response.constant;
-        constantCreated.instructions.push(response.instruction);
-
-        console.log(constantCreated)
     }
+
+    console.log(constantsVerifieds)
 }
 
 verifyConstant();
